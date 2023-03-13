8d9ce402bece60c25daf8a26d6b8b8cdb557e744
unknown <L.Lyubitskaya@gmail.com>
2023-03-13 10:10:45 +0300

    all files commit

---
+++ IdeaProjects/com_arm_view/src/main/java/ru/tnt/corp/com_arm_view/ComArmViewApplication.java
@@ -0,0 +1,13 @@
+package ru.tnt.corp.com_arm_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComArmViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComArmViewApplication.class, args);
+    }
+
+}
